241ff159e54ead94e3ad2053aef7dfe386c818e1
jakubwawrzyczek <jakubwawrzyczek.contact@gmail.com>
2022-06-09 15:18:34 +0200

    1.01

--- main.py
+++ main.py
@@ -1,61 +1,65 @@
 import tkinter as tk
+from tkinter import *
 import os
 
 root = tk.Tk()
 
-while True:
-    instruction = input('Wpisz działanie do wykonania, jeżeli potrzebujesz pomocy wpisz \"help\": ')
-    instruction = instruction.split()
-    if instruction[0] == 'first':
-        if instruction[1] == 'setscore':
-            first_score = open('first_score.txt', 'w+')
-            first_score.write(instruction[2])
-            first_score.close()
-        elif instruction[1] == 'setname':
-            first_name = open('first_name.txt', 'w')
-            first_name.write(instruction[2])
-            first_name.close()
-        else:
-            print('Nie rozpoznaje takiej instrukcji!')
-    elif instruction[0] == 'second':
-        if instruction[1] == 'setscore':
-            second_score = open('second_score.txt', 'w+')
-            second_score.write(instruction[2])
-            second_score.close()
-        elif instruction[1] == 'setname':
-            second_name = open('second_name.txt', 'w')
-            second_name.write(instruction[2])
-            second_name.close()
-        else:
-            print('Nie rozpoznaje takiej instrukcji!')
-    elif instruction[0] == 'timer':
-        if instruction[1] == 'start':
-            os.system('python timer.py')
-        elif instruction[1] == 'reset':
-            timer = open('timer.txt', 'w')
-            timer.write('00:00')
-        else:
-            print('Nie rozpoznaje takiej instrukcji!')
-    elif instruction[0] == 'warn':
-        if instruction[1] == 'red':
-            red_cards = open('red_cards', 'a')
-            red_cards.write(instruction[2] + ' ' + instruction[3] + ' ' + instruction[4] + '\n')
-            red_cards.close()
-        elif instruction[1] == 'yellow':
-            yellow_cards = open('yellow_cards', 'a')
-            yellow_cards.writelines(instruction[2] + ' ' + instruction[3] + ' ' + instruction[4] + '\n')
-            yellow_cards.close()
-        elif instruction[1] == 'reset':
-            yellow_cards = open('yellow_cards', 'w')
-            red_cards = open('red_cards', 'w')
-            red_cards.write('')
-            yellow_cards.write('')
-            yellow_cards.close()
-            red_cards.close()
+fSc = tk.IntVar()
+sSc = tk.IntVar()
+fNm = tk.StringVar()
+sNm = tk.StringVar()
 
-    else:
-        print('Nie rozpoznaje takiej instrukcji!')
+
+def refreshAction():
+    first_score = open('first_score.txt', 'w+')
+    first_score.write(str(fSc.get()))
+    first_score.close()
+
+    second_score = open('second_score.txt', 'w+')
+    second_score.write(str(sSc.get()))
+    second_score.close()
+
+    first_name = open('first_name.txt', 'w')
+    first_name.write(str(fNm.get()))
+    first_name.close()
+
+    first_name = open('second_name.txt', 'w')
+    first_name.write(str(sNm.get()))
+    first_name.close()
+
+
+frame = tk.Frame(root)
+root.title("scoreboard manager")
+frame.pack()
+
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side=TOP)
+
+name1 = tk.Label(frame, text="Team 1")
+name1.pack(side=LEFT)
+
+e1 = tk.Entry(frame, textvariable=fNm)
+e1.pack(side=LEFT)
+
+name2 = tk.Label(bottomFrame, text="Team 2")
+name2.pack(side=LEFT)
+
+e2 = tk.Entry(bottomFrame, textvariable=sNm)
+e2.pack(side=LEFT)
+
+score1 = tk.Label(frame, text="Score")
+score1.pack(side=LEFT)
+
+e3 = tk.Entry(frame, textvariable=fSc)
+e3.pack(side=LEFT)
+
+score2 = tk.Label(bottomFrame, text="Score")
+score2.pack(side=LEFT)
+
+e4 = tk.Entry(bottomFrame, textvariable=sSc)
+e4.pack(side=LEFT)
+
+btn = tk.Button(text="refresh", command=refreshAction)
+btn.pack()
 
 root.mainloop()
-
-#async do obczajenia, jesli nie bedzie dzialac to z osobnego pliku huj dupa cycki dupa cyce wadowice
